1a7fcb53eb68c20bf43ade580cd4f9a16b3fb9c0
MelkyFB <melkysalem@gmail.com>
2019-12-10 14:49:01 +0100

    implementing role tests

--- src/test/kotlin/io/github/melkyfb/worksharebackend/repositories/RoleRepositoryIntegrationTest.kt
+++ src/test/kotlin/io/github/melkyfb/worksharebackend/repositories/RoleRepositoryIntegrationTest.kt
@@ -1,17 +1,85 @@
 package io.github.melkyfb.worksharebackend.repositories
 
+import com.google.gson.Gson
+import io.github.melkyfb.worksharebackend.entities.Role
+import io.github.melkyfb.worksharebackend.entities.User
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithUserDetails
 import org.springframework.test.context.junit4.SpringRunner
-import javax.persistence.Entity
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @RunWith(SpringRunner::class)
-@DataJpaTest
-class RoleRepositoryIntegrationTest(
-    val roleRepository: RoleRepository
-) {
-    fun findByUsername_thenReturnUser() {
-        assert(value = true)
+@SpringBootTest
+@AutoConfigureMockMvc
+class RoleRepositoryIntegrationTest {
+
+    @Autowired
+    lateinit var roleRepository: RoleRepository
+
+    @Autowired
+    lateinit var mvc: MockMvc
+
+    @Test
+    fun contextLoadedFine() {
+        assertThat(roleRepository).isNotNull()
+        assertThat(mvc).isNotNull
     }
+
+    @Test
+    fun unauthorizedWhenNoCreds() {
+        mvc.perform(MockMvcRequestBuilders.get("/roles"))
+            .andDo(print())
+            .andExpect(status().isUnauthorized)
+    }
+
+    @Test
+    @WithUserDetails("admin")
+    fun authorizedWhenCreds() {
+        mvc.perform(
+            MockMvcRequestBuilders
+                .get("/roles").accept(MediaType.APPLICATION_JSON)
+
+        )
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+    }
+
+    @Test
+    @WithUserDetails("admin")
+    fun createUser() {
+        var role = Role(
+            null,
+            "roleTest",
+            "descriptionTest",
+            null
+        )
+        var gson = Gson()
+        var roleJson = gson.toJson(role)
+
+        mvc.perform(
+            MockMvcRequestBuilders
+                .post("/roles")
+                .content(roleJson)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andDo(print())
+            .andExpect(status().isCreated)
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.role").value("roleTest"))
+            .andExpect(jsonPath("$.description").value("descriptionTest"))
+    }
+
 }
